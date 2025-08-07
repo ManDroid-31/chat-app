70529ce2067904eef628fb02c6b2d5c05dd64c76
Manas_on_this_terminal <manasshinde2005@gmail.com>
2025-08-08 00:15:16 +0530

    fix: remove accidental tracking of server/.gitignore from client repo

--- client/src/utils/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useSelector((state) => state.auth);
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-}
-
-export default ProtectedRoute;
